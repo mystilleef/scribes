fdc3d1895d242b842b7ffe7b0cf0a6b3a66d78e5
goldenmyst <goldenmyst@goldenmyst-desktop>
2008-04-04 09:59:17 -0400

    Implemented function to close current window and reopen new one.

---
+++ plugins/CloseReopen/Trigger.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2008 Lateef Alabi-Oki
+#
+# This file is part of Scribes.
+#
+# Scribes is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Scribes is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Scribes; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+# USA
+
+"""
+This module documents a class that creates a trigger that closes the
+current window and opens a new one.
+
+@author: Lateef Alabi-Oki
+@organization: The Scribes Project
+@copyright: Copyright © 2008 Lateef Alabi-Oki
+@license: GNU GPLv3 or Later
+@contact: mystilleef@gmail.com
+"""
+
+class Trigger(object):
+	"""
+	This class creates an object, a trigger, that closes the current
+	window and opens a new one.
+	"""
+
+	def __init__(self, editor):
+		"""
+		Initialize the trigger.
+
+		@param self: Reference to the Trigger instance.
+		@type self: A Trigger object.
+
+		@param editor: Reference to the text editor.
+		@type editor: An Editor object.
+		"""
+		self.__init_attributes(editor)
+		self.__signal_id_1 = self.__trigger.connect("activate", self.__close_reopen_cb)
+
+	def __init_attributes(self, editor):
+		"""
+		Initialize the trigger's attributes.
+
+		@param self: Reference to the Trigger instance.
+		@type self: A Trigger object.
+
+		@param editor: Reference to the text editor.
+		@type editor: An Editor object.
+		"""
+		self.__editor = editor
+		self.__trigger = self.__create_trigger()
+		self.__signal_id_1 = None
+		return
+
+	def __create_trigger(self):
+		"""
+		Create the trigger.
+
+		@param self: Reference to the Trigger instance.
+		@type self: A Trigger object.
+		"""
+		# Trigger to close current window and open new one.
+		self.__trigger = self.__editor.create_trigger("close_reopen", "ctrl - N")
+		self.__editor.add_trigger(self.__trigger)
+		return self.__trigger
+
+	def __close_reopen_cb(self, *args):
+		"""
+		Handles callback when the "activate" signal is emitted.
+
+		@param self: Reference to the Trigger instance.
+		@type self: A Trigger object.
+
+		@param trigger: Trigger that closes current window and reopens new one.
+		@type trigger: A Trigger object.
+		"""
+		self.__editor.trigger("new_window")
+		self.__editor.trigger("close_window")
+		return
+
+	def destroy(self):
+		"""
+		Destroy object.
+
+		@param self: Reference to the Trigger instance.
+		@type self: An Trigger object.
+		"""
+		self.__editor.remove_trigger(self.__trigger)
+		self.__editor.disconnect_signal(self.__signal_id_1, self.__trigger)
+		del self
+		self = None
+		return
