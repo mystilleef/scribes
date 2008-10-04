76763e8ebfca99ed0f8c6060fad7fd27fd6d8b88
meek <meek@sobmishon>
2008-10-04 14:49:15 -0400

    Ported python syntax error checker to new API. version bump

--- LanguagePlugins/PythonSyntaxErrorChecker/Trigger.py
+++ LanguagePlugins/PythonSyntaxErrorChecker/Trigger.py
@@ -1,33 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright © 2008 Lateef Alabi-Oki
-#
-# This file is part of Scribes.
-#
-# Scribes is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# Scribes is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Scribes; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
-# USA
-
-"""
-This module documents a class that checks Python source code for syntax
-errors.
-
-@author: Lateef Alabi-Oki
-@organization: The Scribes Project
-@copyright: Copyright © 2008 Lateef Alabi-Oki
-@license: GNU GPLv3 or Later
-@contact: mystilleef@gmail.com
-"""
 
 class Trigger(object):
 	"""
@@ -36,56 +6,22 @@ class Trigger(object):
 	"""
 
 	def __init__(self, editor):
-		"""
-		Initialize the trigger.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-
-		@param editor: Reference to the text editor.
-		@type editor: An Editor object.
-		"""
 		self.__init_attributes(editor)
-		self.__signal_id_1 = self.__trigger.connect("activate", self.__check_error_cb)
+		self.__sigid1 = self.__trigger.connect("activate", self.__check_error_cb)
 
 	def __init_attributes(self, editor):
-		"""
-		Initialize the trigger's attributes.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-
-		@param editor: Reference to the text editor.
-		@type editor: An Editor object.
-		"""
 		self.__editor = editor
 		self.__manager = None
-		self.__trigger = self.__create_trigger()
-		self.__signal_id_1 = None
+		self.__trigger = self.__create_trigger("check_syntax_errors", "F2")
 		return
 
-	def __create_trigger(self):
-		"""
-		Create the trigger.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-		"""
+	def __create_trigger(self, name, shortcut):
 		# Trigger to check for syntax errors.
-		self.__trigger = self.__editor.create_trigger("check_syntax_errors", "F2")
-		self.__editor.add_trigger(self.__trigger)
-		return self.__trigger
+		trigger = self.__editor.create_trigger(name, shortcut)
+		self.__editor.add_trigger(trigger)
+		return trigger
 
 	def __check_error_cb(self, *args):
-		"""
-		Handles callback when the "activate" signal is emitted.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-
-		@param trigger: An object to show the symbol browser.
-		@type trigger: A Trigger object.
-		"""
 		try:
 			self.__manager.check()
 		except AttributeError:
@@ -95,14 +31,8 @@ class Trigger(object):
 		return
 
 	def destroy(self):
-		"""
-		Destroy object.
-
-		@param self: Reference to the Trigger instance.
-		@type self: An Trigger object.
-		"""
 		self.__editor.remove_trigger(self.__trigger)
-		self.__editor.disconnect_signal(self.__signal_id_1, self.__trigger)
+		self.__editor.disconnect_signal(self.__sigid1, self.__trigger)
 		if self.__manager: self.__manager.destroy()
 		del self
 		self = None
