61ca4c6dee731522c2ea3c76be1efe75175197cb
meek <meek@sobmishon>
2008-11-29 12:26:25 -0500

    Ported close reopen plugin to the new API and coding style. Press ctrl+shift+n to close the current window and open a new one. version bump

--- plugins/CloseReopen/Trigger.py
+++ plugins/CloseReopen/Trigger.py
@@ -1,72 +1,31 @@
 class Trigger(object):
-	"""
-	This class creates an object, a trigger, that closes the current
-	window and opens a new one.
-	"""
 
 	def __init__(self, editor):
-		"""
-		Initialize the trigger.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-
-		@param editor: Reference to the text editor.
-		@type editor: An Editor object.
-		"""
 		self.__init_attributes(editor)
-		self.__signal_id_1 = self.__trigger.connect("activate", self.__close_reopen_cb)
+		self.__sigid1 = self.__trigger.connect("activate", self.__activate_cb)
 
 	def __init_attributes(self, editor):
-		"""
-		Initialize the trigger's attributes.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-
-		@param editor: Reference to the text editor.
-		@type editor: An Editor object.
-		"""
 		self.__editor = editor
 		self.__trigger = self.__create_trigger()
-		self.__signal_id_1 = None
 		return
 
 	def __create_trigger(self):
-		"""
-		Create the trigger.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-		"""
 		# Trigger to close current window and open new one.
-		self.__trigger = self.__editor.create_trigger("close_reopen", "ctrl+shift+n")
-		self.__editor.add_trigger(self.__trigger)
-		return self.__trigger
+		trigger = self.__editor.create_trigger("close_reopen", "ctrl+shift+n")
+		self.__editor.add_trigger(trigger)
+		return trigger
 
-	def __close_reopen_cb(self, *args):
-		"""
-		Handles callback when the "activate" signal is emitted.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-
-		@param trigger: Trigger that closes current window and reopens new one.
-		@type trigger: A Trigger object.
-		"""
-		self.__editor.trigger("new_window")
-		self.__editor.trigger("close_window")
+	def __activate_cb(self, *args):
+		self.__editor.response()
+		self.__editor.new()
+		self.__editor.response()
+		self.__editor.close()
+		self.__editor.response()
 		return
 
 	def destroy(self):
-		"""
-		Destroy object.
-
-		@param self: Reference to the Trigger instance.
-		@type self: An Trigger object.
-		"""
 		self.__editor.remove_trigger(self.__trigger)
-		self.__editor.disconnect_signal(self.__signal_id_1, self.__trigger)
+		self.__editor.disconnect_signal(self.__sigid1, self.__trigger)
 		del self
 		self = None
 		return
--- plugins/PluginCloseReopen.py
+++ plugins/PluginCloseReopen.py
@@ -1,76 +1,21 @@
-# -*- coding: utf-8 -*-
-# Copyright © 2008 Lateef Alabi-Oki
-#
-# This file is part of Scribes.
-#
-# Scribes is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# Scribes is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Scribes; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
-# USA
-
-"""
-This module documents a class that loads the about plugin.
-
-@author: Lateef Alabi-Oki
-@organization: The Scribes Project
-@copyright: Copyright © 2008 Lateef Alabi-Oki
-@license: GNU GPLv3 or Later
-@contact: mystilleef@gmail.com
-"""
-
 name = "CloseReopen Plugin"
 authors = ["Lateef Alabi-Oki <mystilleef@gmail.com>"]
-version = 0.1
-autoload = False
+version = 0.2
+autoload = True
 class_name = "CloseReopenPlugin"
 short_description = "Close current window, reopen new one."
 long_description = """Close current window, reopen new one."""
 
 class CloseReopenPlugin(object):
-	"""
-	This class loads the plugin that closes the current window and opens
-	a new one.
-	"""
 
 	def __init__(self, editor):
-		"""
-		Initialize object.
-
-		@param self: Reference to the CloseReopenPlugin loader.
-		@type self: An CloseReopen object.
-
-		@param editor: Reference to the text editor.
-		@type editor: An Editor object.
-		"""
 		self.__editor = editor
 
 	def load(self):
-		"""
-		Load the about plugin.
-
-		@param self: Reference to the CloseReopenPlugin loader.
-		@type self: An CloseReopenPlugin object.
-		"""
 		from CloseReopen.Trigger import Trigger
 		self.__trigger = Trigger(self.__editor)
 		return
 
 	def unload(self):
-		"""
-		Unload the about plugin.
-
-		@param self: Reference to the CloseReopenPlugin loader.
-		@type self: An CloseReopenPlugin object.
-		"""
 		self.__trigger.destroy()
 		return
