865e9b8eb99d6bd91e414f4bb5e2b512bb56d09c
meek <meek@sobmishon>
2008-12-10 14:04:17 -0500

    Press ctrl+alt+m to toggle minimal interface mode. When minimal mode is enable move the cursor towards the toolbar area to show the full interface temporarily.

--- SCRIBES/Globals.py
+++ SCRIBES/Globals.py
@@ -48,7 +48,7 @@ data_folder = data_path + "/scribes"
 core_plugin_folder = data_folder + "/plugins"
 core_language_plugin_folder = data_folder + "/LanguagePlugins"
 python_path = "/usr/lib/python2.5/site-packages"
-version = "0.4-dev-build336"
+version = "0.4-dev-build339"
 author = ["Author:", "\tLateef Alabi-Oki <mystilleef@gmail.com>\n",
 			"Contributors:",
 			"\tIb Lundgren <ib.lundgren@gmail.com>",
