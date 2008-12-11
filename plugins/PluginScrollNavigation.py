3df87b9195be77dd742c09f356c73edc76fe50ab
meek <meek@sobmishon>
2008-12-11 16:29:11 -0500

    Ported scroll navigation plugin to new API and coding style. ctrl+Up to scroll up ctrl+Down to scroll down and alt+m to center buffer

--- plugins/PluginScrollNavigation.py
+++ plugins/PluginScrollNavigation.py
@@ -1,78 +1,22 @@
-# -*- coding: utf-8 -*-
-# Copyright © 2007 Lateef Alabi-Oki
-#
-# This file is part of Scribes.
-#
-# Scribes is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# Scribes is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Scribes; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
-# USA
-
-"""
-This module documents a class that loads the scroll navigation
-plugin.
-
-@author: Lateef Alabi-Oki
-@organization: The Scribes Project
-@copyright: Copyright © 2005 Lateef Alabi-Oki
-@license: GNU GPLv2 or Later
-@contact: <mystilleef@gmail.com>
-"""
-
 name = "Scroll Navigation Plugin"
 authors = ["Lateef Alabi-Oki <mystilleef@gmail.com>"]
-version = 0.1
-autoload = False
+version = 0.2
+autoload = True
 class_name = "ScrollNavigationPlugin"
 short_description = "Keyboard Scroll navigation for Scribes."
 long_description = """Keyboard Scroll navigation for Scribes."""
 
 class ScrollNavigationPlugin(object):
-	"""
-	This class initializes a plug-in that performs scroll navigation
-	via the keyboard.
-	"""
 
 	def __init__(self, editor):
-		"""
-		Initialize the plug-in object.
-
-		@param self: Reference to the ScrollNavigationPlugin instance.
-		@type self: A ScrollNavigationPlugin object.
-
-		@param editor: Reference to the text editor.
-		@type editor: An Editor object.
-		"""
 		self.__editor = editor
 		self.__trigger = None
 
 	def load(self):
-		"""
-		Initialize the ScrollNavigationPlugin instance.
-
-		@param self: Reference to the ScrollNavigationPlugin instance.
-		@type self: An ScrollNavigationPlugin object.
-		"""
 		from ScrollNavigation.Trigger import Trigger
 		self.__trigger = Trigger(self.__editor)
 		return
 
 	def unload(self):
-		"""
-		Destroy the ScrollNavigationPlugin instance.
-
-		@param self: Reference to the ScrollNavigationPlugin instance.
-		@type self: An ScrollNavigationPlugin object.
-		"""
 		self.__trigger.destroy()
 		return
--- plugins/ScrollNavigation/Trigger.py
+++ plugins/ScrollNavigation/Trigger.py
@@ -2,108 +2,48 @@ class Trigger(object):
 
 	def __init__(self, editor):
 		self.__init_attributes(editor)
-		self.__create_triggers()
-		self.__signal_id_1 = self.__up_trigger.connect("activate", self.__up_cb)
-		self.__signal_id_2 = self.__down_trigger.connect("activate", self.__down_cb)
-		self.__signal_id_3 = self.__middle_trigger.connect("activate", self.__middle_cb)
+		self.__sigid1 = self.__trigger1.connect("activate", self.__activate_cb)
+		self.__sigid2 = self.__trigger2.connect("activate", self.__activate_cb)
+		self.__sigid3 = self.__trigger3.connect("activate", self.__activate_cb)
 
 	def __init_attributes(self, editor):
 		self.__editor = editor
 		self.__manager = None
-		return
-
-	def __create_triggers(self):
-		# Trigger to scroll up.
-		self.__up_trigger = self.__editor.create_trigger("scroll_up", "ctrl+Up")
-		self.__editor.add_trigger(self.__up_trigger)
-
-		# Trigger to scroll down.
-		self.__down_trigger = self.__editor.create_trigger("scroll_down", "ctrl+Down")
-		self.__editor.add_trigger(self.__down_trigger)
-
-		# Trigger to center current line.
-		self.__middle_trigger = self.__editor.create_trigger("center", "alt+m")
-		self.__editor.add_trigger(self.__middle_trigger)
-		return
-
-	def __up_cb(self, trigger):
-		"""
-		Handles callback when the "activate" signal is emitted.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-
-		@param trigger: An object to show the document browser.
-		@type trigger: A Trigger object.
-		"""
-		try:
-			self.__manager.scroll_up()
-		except AttributeError:
-			from Manager import Manager
-			self.__manager = Manager(self.__editor)
-			self.__manager.scroll_up()
-		return
-
-	def __down_cb(self, trigger):
-		"""
-		Handles callback when the "activate" signal is emitted.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-
-		@param trigger: An object to show the document browser.
-		@type trigger: A Trigger object.
-		"""
-		try:
-			self.__manager.scroll_down()
-		except AttributeError:
-			from Manager import Manager
-			self.__manager = Manager(self.__editor)
-			self.__manager.scroll_down()
-		return
-
-	def __middle_cb(self, trigger):
-		"""
-		Handles callback when the "activate" signal is emitted.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-
-		@param trigger: An object to show the document browser.
-		@type trigger: A Trigger object.
-		"""
-		try:
-			self.__manager.center()
-		except AttributeError:
-			from Manager import Manager
-			self.__manager = Manager(self.__editor)
-			self.__manager.center()
+		self.__trigger1 = self.__create_trigger("scroll_up", "ctrl+Up")
+		self.__trigger2 = self.__create_trigger("scroll_down", "ctrl+Down")
+		self.__trigger3 = self.__create_trigger("center", "alt+m")
 		return
 
 	def __destroy(self):
-		"""
-		Destroy trigger.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-		"""
-		self.__editor.disconnect_signal(self.__signal_id_1, self.__up_trigger)
-		self.__editor.disconnect_signal(self.__signal_id_2, self.__down_trigger)
-		self.__editor.disconnect_signal(self.__signal_id_3, self.__middle_trigger)
-		self.__editor.remove_trigger(self.__up_trigger)
-		self.__editor.remove_trigger(self.__down_trigger)
-		self.__editor.remove_trigger(self.__middle_trigger)
 		if self.__manager: self.__manager.destroy()
+		self.__editor.disconnect_signal(self.__sigid1, self.__trigger1)
+		self.__editor.disconnect_signal(self.__sigid2, self.__trigger2)
+		self.__editor.disconnect_signal(self.__sigid3, self.__trigger3)
+		triggers = (self.__trigger1, self.__trigger2, self.__trigger3)
+		self.__editor.remove_triggers(triggers)
 		del self
 		self = None
+		return False
+
+	def __create_trigger(self, name, shortcut):
+		trigger = self.__editor.create_trigger(name, shortcut)
+		self.__editor.add_trigger(trigger)
+		return trigger
+
+	def __get_manager(self):
+		from Manager import Manager
+		return Manager(self.__editor)
+
+	def __activate_cb(self, trigger):
+		if not self.__manager: self.__manager = self.__get_manager()
+		function = {
+			self.__trigger1: self.__manager.scroll_up,
+			self.__trigger2: self.__manager.scroll_down,
+			self.__trigger3: self.__manager.center,
+		}
+		function[trigger]()
 		return
 
 	def destroy(self):
-		"""
-		Destroy trigger.
-
-		@param self: Reference to the Trigger instance.
-		@type self: A Trigger object.
-		"""
 		self.__destroy()
 		return
