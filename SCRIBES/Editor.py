ab8f22eec5669d04b939a08377e33d9511e8f26c
azibioki <azibioki@azibioki>
2009-03-01 10:32:50 -0500

    Moved Editor signals to a new module, SIGNALS.py

--- SCRIBES/Editor.py
+++ SCRIBES/Editor.py
@@ -1,6 +1,3 @@
-from gobject import GObject, SIGNAL_RUN_LAST, TYPE_NONE, TYPE_BOOLEAN
-from gobject import TYPE_STRING, TYPE_OBJECT, TYPE_PYOBJECT, TYPE_INT
-from gobject import SIGNAL_ACTION, type_register, SIGNAL_NO_RECURSE
 from Globals import data_folder, metadata_folder, home_folder, desktop_folder
 from Globals import session_bus, core_plugin_folder, home_plugin_folder
 from Globals import home_language_plugin_folder, core_language_plugin_folder
@@ -16,77 +13,13 @@ from EncodingMetadata import get_value as get_encoding_list
 from Utils import get_language, word_pattern
 from SupportedEncodings import get_supported_encodings
 from gettext import gettext as _
-SSIGNAL = SIGNAL_RUN_LAST|SIGNAL_NO_RECURSE|SIGNAL_ACTION
-SACTION = SIGNAL_RUN_LAST|SIGNAL_ACTION
 
-class Editor(GObject):
+from SIGNALS import Signals
 
-	__gsignals__ = {
-		# Nobody should listen to this signal. For internal use only.
-		"close": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
-		# QUIT signal to all core objects. This signal is emitted only after
-		# a file has been properly saved. For internal use only. PlEASE NEVER
-		# EMIT THIS SIGNAL. This is the signal to listen to for proper cleanup
-		# before exit.
-		"quit": (SSIGNAL, TYPE_NONE, ()),
-		"cursor-moved": (SSIGNAL, TYPE_NONE, ()),
-		"ready": (SSIGNAL, TYPE_NONE, ()),
-		"readonly": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
-		"toggle-readonly": (SSIGNAL, TYPE_NONE, ()),
-		"toggle-fullscreen": (SSIGNAL, TYPE_NONE, ()),
-		"busy": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
-		"private-busy": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
-		"checking-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING,)),
-		"loading-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING,)),
-		"loaded-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
-		"load-file": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT, TYPE_PYOBJECT)),
-		"load-error": (SSIGNAL, TYPE_NONE, (TYPE_STRING,)),
-		"show-error": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING, TYPE_OBJECT, TYPE_BOOLEAN)),
-		"show-info": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING, TYPE_OBJECT, TYPE_BOOLEAN)),
-		"modified-file": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
-		"new-encoding-list": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
-		"update-encoding-guess-list": (SSIGNAL, TYPE_NONE, (TYPE_STRING,)),
-		"renamed-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
-		"rename-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
-		"reload-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
-		"saved-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
-		"save-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
-		"private-save-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
-		"save-error": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING, TYPE_STRING)),
-		"send-data-to-processor": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING
-		)),
-		"private-encoding-load-error": (SSIGNAL, TYPE_NONE, ()),
-		"dbus-saved-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
-		"dbus-save-error": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING, TYPE_STRING)),
-		"window-focus-out": (SSIGNAL, TYPE_NONE, ()),
-		"combobox-encoding-data?": (SSIGNAL, TYPE_NONE, ()),
-		"combobox-encoding-data": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
-		"supported-encodings-window": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
-		"remove-bar-object": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
-		"add-bar-object": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
-		"spin-throbber": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
-		"bar-is-active": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
-		"update-message": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING, TYPE_INT,)),
-		"set-message": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
-		"unset-message": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
-		"undo": (SSIGNAL, TYPE_NONE, ()),
-		"redo": (SSIGNAL, TYPE_NONE, ()),
-		"add-trigger": (SACTION, TYPE_NONE, (TYPE_PYOBJECT,)),
-		"remove-trigger": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
-		"add-triggers": (SACTION, TYPE_NONE, (TYPE_PYOBJECT,)),
-		"remove-triggers": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
-		"register-object": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
-		"unregister-object": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
-		"trigger": (SSIGNAL, TYPE_NONE, (TYPE_STRING,)),
-		"refresh": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
-		"add-to-popup": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
-		"add-to-pref-menu": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
-		"remove-from-pref-menu": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
-		"fullscreen": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
-	}
+class Editor(Signals):
 
 	def __init__(self, manager, uri=None, encoding="utf-8"):
-		GObject.__init__(self)
+		Signals.__init__(self)
 		self.set_data("InstanceManager", manager)
 		from RegistrationManager import Manager
 		Manager(self)
---
+++ SCRIBES/SIGNALS.py
@@ -0,0 +1,74 @@
+from gobject import GObject, SIGNAL_RUN_LAST, TYPE_NONE, TYPE_BOOLEAN
+from gobject import TYPE_STRING, TYPE_OBJECT, TYPE_PYOBJECT, TYPE_INT
+from gobject import SIGNAL_ACTION, type_register, SIGNAL_NO_RECURSE
+SSIGNAL = SIGNAL_RUN_LAST|SIGNAL_NO_RECURSE|SIGNAL_ACTION
+SACTION = SIGNAL_RUN_LAST|SIGNAL_ACTION
+
+class Signals(GObject):
+	
+	__gsignals__ = {
+		# Nobody should listen to this signal. For internal use only.
+		"close": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
+		# QUIT signal to all core objects. This signal is emitted only after
+		# a file has been properly saved. For internal use only. PlEASE NEVER
+		# EMIT THIS SIGNAL. This is the signal to listen to for proper cleanup
+		# before exit.
+		"quit": (SSIGNAL, TYPE_NONE, ()),
+		"cursor-moved": (SSIGNAL, TYPE_NONE, ()),
+		"ready": (SSIGNAL, TYPE_NONE, ()),
+		"readonly": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
+		"toggle-readonly": (SSIGNAL, TYPE_NONE, ()),
+		"toggle-fullscreen": (SSIGNAL, TYPE_NONE, ()),
+		"busy": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
+		"private-busy": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
+		"checking-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING,)),
+		"loading-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING,)),
+		"loaded-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
+		"load-file": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT, TYPE_PYOBJECT)),
+		"load-error": (SSIGNAL, TYPE_NONE, (TYPE_STRING,)),
+		"show-error": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING, TYPE_OBJECT, TYPE_BOOLEAN)),
+		"show-info": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING, TYPE_OBJECT, TYPE_BOOLEAN)),
+		"modified-file": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
+		"new-encoding-list": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
+		"update-encoding-guess-list": (SSIGNAL, TYPE_NONE, (TYPE_STRING,)),
+		"renamed-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
+		"rename-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
+		"reload-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
+		"saved-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
+		"save-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
+		"private-save-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
+		"save-error": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING, TYPE_STRING)),
+		"send-data-to-processor": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING
+		)),
+		"private-encoding-load-error": (SSIGNAL, TYPE_NONE, ()),
+		"dbus-saved-file": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
+		"dbus-save-error": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING, TYPE_STRING)),
+		"window-focus-out": (SSIGNAL, TYPE_NONE, ()),
+		"combobox-encoding-data?": (SSIGNAL, TYPE_NONE, ()),
+		"combobox-encoding-data": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
+		"supported-encodings-window": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
+		"remove-bar-object": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
+		"add-bar-object": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
+		"spin-throbber": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
+		"bar-is-active": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
+		"update-message": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING, TYPE_INT,)),
+		"set-message": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
+		"unset-message": (SSIGNAL, TYPE_NONE, (TYPE_STRING, TYPE_STRING)),
+		"undo": (SSIGNAL, TYPE_NONE, ()),
+		"redo": (SSIGNAL, TYPE_NONE, ()),
+		"add-trigger": (SACTION, TYPE_NONE, (TYPE_PYOBJECT,)),
+		"remove-trigger": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
+		"add-triggers": (SACTION, TYPE_NONE, (TYPE_PYOBJECT,)),
+		"remove-triggers": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
+		"register-object": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
+		"unregister-object": (SSIGNAL, TYPE_NONE, (TYPE_PYOBJECT,)),
+		"trigger": (SSIGNAL, TYPE_NONE, (TYPE_STRING,)),
+		"refresh": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
+		"add-to-popup": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
+		"add-to-pref-menu": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
+		"remove-from-pref-menu": (SSIGNAL, TYPE_NONE, (TYPE_OBJECT,)),
+		"fullscreen": (SSIGNAL, TYPE_NONE, (TYPE_BOOLEAN,)),
+	}
+
+	def __init__(self):
+		GObject.__init__(self)
