337bfa49530e2093f3606af79343ef5e31b3a96a
azibioki <azibioki@azibioki>
2009-03-02 09:30:45 -0500

    Removed redundant comment headers from Window.py

--- SCRIBES/Window.py
+++ SCRIBES/Window.py
@@ -1,33 +1,3 @@
-﻿# -*- coding: utf-8 -*-
-# Copyright © 2005 Lateef Alabi-Oki
-#
-# This file is part of Scribes.
-#
-# Scribes is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# Scribes is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Scribes; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-"""
-This module implements a class responsible for creating a window object for
-text editor instances.
-
-@author: Lateef Alabi-Oki
-@organiation: The Scribes Project
-@copyright: Copyright © 2005 Lateef Alabi-Oki
-@license: GNU GPLv2 or Later
-@contact: mystilleef@gmail.com
-"""
-
 from gettext import gettext as _
 
 class Window(object):
