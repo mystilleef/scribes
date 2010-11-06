cbcff7f73342f9121b233f8c8d1725cd2adee59b
lateef <mystilleef@gmail.com>
2010-11-06 08:05:46 -0400

    Enable editing area immediately after all GUI components have been initialized or immediately after loading.

--- SCRIBES/PluginSystemInitializer.py
+++ SCRIBES/PluginSystemInitializer.py
@@ -5,8 +5,11 @@ class Initializer(SignalManager):
 	def __init__(self, editor, uri):
 		SignalManager.__init__(self, editor)
 		self.__init_attributes(editor)
-		self.connect(editor, "loaded-file", self.__loaded_cb, True)
+		self.connect(editor, "loaded-file", self.__loaded_cb)
+		self.connect(editor, "loaded-file", self.__loaded_after_cb, True)
 		self.connect(editor, "load-error", self.__loaded_cb)
+		self.connect(editor, "load-error", self.__loaded_after_cb, True)
+		if not uri: self.__ready()
 		if not uri: self.__init_plugins()
 
 	def __init_attributes(self, editor):
@@ -21,10 +24,13 @@ class Initializer(SignalManager):
 	def __init_plugins(self):
 		from PluginInitializer.Manager import Manager
 		Manager(self.__editor)
-		self.__editor.emit("ready")
-		self.__editor.textview.grab_focus()
 		self.__destroy()
 		return False
+
+	def __ready(self):
+		self.__editor.emit("ready")
+		self.__editor.textview.grab_focus()
+		return
 
 	def __init_plugins_on_idle(self):
 		from gobject import idle_add, PRIORITY_LOW
@@ -32,6 +38,10 @@ class Initializer(SignalManager):
 		return False
 
 	def __loaded_cb(self, *args):
+		self.__ready()
+		return False
+
+	def __loaded_after_cb(self, *args):
 		from gobject import timeout_add, PRIORITY_LOW
-		timeout_add(1000, self.__init_plugins, priority=PRIORITY_LOW)
+		timeout_add(1000, self.__init_plugins_on_idle, priority=PRIORITY_LOW)
 		return False
