67e6559f18da1059ac89c404d63898315155e538
Lateef Alabi-Oki <mystilleef@gmail.com>
2010-12-18 22:33:49 -0500

    Use ctrl+shift+comma to show zen coding interface.

--- LanguagePlugins/zencoding/Trigger.py
+++ LanguagePlugins/zencoding/Trigger.py
@@ -96,7 +96,7 @@ class Trigger(SignalManager, TriggerManager):
 		self.__trigger9.zen_action = "merge_lines"
 		name, shortcut, description, category = (
 			"zencoding-wrap-with-abbreviation",
-			"<ctrl><shift>e",
+			"<ctrl><shift>comma",
 			_("Wrap with abbreviation"),
 			_("Markup Operations")
 		)
