85469c07e0f3852dc7e1d82eb7346adc42c76a2c
Lateef Alabi-Oki <mystilleef@gmail.com>
2011-02-21 07:31:34 -0500

    Fork save process

--- SCRIBES/SaveSystem/ExternalProcess/ScribesSaveProcess.py
+++ SCRIBES/SaveSystem/ExternalProcess/ScribesSaveProcess.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == "__main__":
+	from SCRIBES.Utils import fork_process
+	fork_process()
 	from sys import argv, path
 	path.insert(0, argv[1])
 	from signal import signal, SIGINT, SIG_IGN
