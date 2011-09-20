1694fc22ea0163792dd00b6da474035412ca6b57
Lateef Alabi-Oki <mystilleef@gmail.com>
2011-09-20 12:26:33 -0400

    Properly initialize attributes in the file change monitoring modules. Otherwise, Python, or PyGObject, discards them cause bugs.

--- SCRIBES/FileChangeMonitor.py
+++ SCRIBES/FileChangeMonitor.py
@@ -5,25 +5,28 @@ class Monitor(SignalManager):
 	def __init__(self, editor):
 		SignalManager.__init__(self)
 		self.__init_attributes(editor)
-		self.connect(editor, "quit", self.__quit_cb)
 		self.connect(editor, "close", self.__close_cb)
-		self.connect(editor, "loaded-file", self.__monitor_cb, True)
-		self.connect(editor, "renamed-file", self.__monitor_cb, True)
+		self.connect(editor, "quit", self.__quit_cb)
 		self.connect(editor, "save-file", self.__busy_cb)
 		self.connect(editor, "rename-file", self.__busy_cb)
+		self.connect(editor, "saved-file", self.__saved_file_cb)
 		self.connect(editor, "save-error", self.__nobusy_cb, True)
 		self.connect(editor, "saved-file", self.__nobusy_cb, True)
-		self.connect(editor, "saved-file", self.__saved_file_cb)
+		self.connect(editor, "loaded-file", self.__monitor_cb, True)
+		self.connect(editor, "renamed-file", self.__monitor_cb, True)
 		editor.register_object(self)
+		# print "Initializing file change monitoring for ", self.__editor.id_
 
 	def __init_attributes(self, editor):
 		self.__editor = editor
 		self.__uri = ""
-		self.__file_monitor = None
+		self.__file_monitor = ""
+		self.__is_monitoring = False
+		self.__timer1, self.__timer2 = "", ""
 		return
 
 	def __destroy(self):
-		self.__unmonitor(self.__uri)
+		# print "Destroying file change monitor for ", self.__editor.id_
 		self.disconnect()
 		self.__editor.unregister_object(self)
 		del self
@@ -35,19 +38,23 @@ class Monitor(SignalManager):
 		from gio import File, FILE_MONITOR_NONE
 		self.__file_monitor = File(uri).monitor_file(FILE_MONITOR_NONE, None)
 		self.__file_monitor.connect("changed", self.__changed_cb)
+		self.__is_monitoring = True
+		# print "#" * 80
+		# print "Started monitoring ", self.__editor.id_
 		return False
 
 	def __unmonitor(self, uri):
-		try:
-			if not uri or not self.__file_monitor: return False
-			self.__file_monitor.cancel()
-		except AttributeError:
-			pass
+		if not self.__file_monitor or self.__is_monitoring is False: return False
+		self.__file_monitor.cancel()
+		self.__is_monitoring = False
+		# print "Stopped monitoring ", self.__editor.id_
+		# print "#" * 80
 		return False
 
 	def __reload(self):
 		from URILoader.Manager import Manager
 		Manager(self.__editor, self.__editor.uri, self.__editor.encoding)
+		# print "File change detected! Reloading ", self.__uri
 		return False
 
 	def __remove_timer(self, _timer=1):
@@ -56,10 +63,15 @@ class Monitor(SignalManager):
 				1: self.__timer1,
 				2: self.__timer2,
 			}
+			# print "source timer ", _timer
 			from gobject import source_remove
 			source_remove(timers[_timer])
-		except AttributeError:
+		except TypeError:
 			pass
+		return False
+
+	def __remove_all_timers(self):
+		[self.__remove_timer(_timer) for _timer in xrange(1, 3)]
 		return False
 
 	def __quit_cb(self, *args):
@@ -72,17 +84,19 @@ class Monitor(SignalManager):
 		return False
 
 	def __changed_cb(self, *args):
-		self.__remove_timer(1)
+		self.__remove_all_timers()
+		self.__unmonitor(self.__uri)
 		from gobject import timeout_add, PRIORITY_LOW
 		self.__timer1 = timeout_add(1500, self.__reload, priority=PRIORITY_LOW)
 		return False
 
 	def __busy_cb(self, *args):
+		self.__remove_all_timers()
 		self.__unmonitor(self.__uri)
 		return False
 
 	def __nobusy_cb(self, *args):
-		self.__remove_timer(2)
+		self.__remove_all_timers()
 		from gobject import timeout_add, PRIORITY_LOW
 		self.__timer2 = timeout_add(1000, self.__monitor, self.__uri, priority=PRIORITY_LOW)
 		return False
@@ -92,7 +106,7 @@ class Monitor(SignalManager):
 		return False
 
 	def __close_cb(self, *args):
+		self.__remove_all_timers()
 		self.__unmonitor(self.__uri)
-		self.__remove_timer(1)
-		self.__remove_timer(2)
+		# print "Stop monitoring and closing all timers"
 		return False
