827550cadcb7a871f12bff32b0f0e56ad6256dcd
Lateef Alabi-Oki <mystilleef@gmail.com>
2011-09-21 06:37:54 -0400

    Removed session-id state from session completer module.

--- SCRIBES/SaveSystem/SessionCompleter.py
+++ SCRIBES/SaveSystem/SessionCompleter.py
@@ -6,7 +6,6 @@ class Completer(SignalManager):
 		SignalManager.__init__(self)
 		self.__init_attributes(manager, editor)
 		self.connect(editor, "quit", self.__quit_cb)
-		self.connect(manager, "session-id", self.__session_cb)
 		self.connect(manager, "save-succeeded", self.__data_cb)
 		self.connect(manager, "save-failed", self.__data_cb)
 		editor.register_object(self)
@@ -14,28 +13,12 @@ class Completer(SignalManager):
 	def __init_attributes(self, manager, editor):
 		self.__manager = manager
 		self.__editor = editor
-		from collections import deque
-		self.__queue = deque()
-		self.__session_id = ()
 		return
 
 	def __verify_session(self, data):
-		try:
-			session_id = tuple(data[0])
-			self.__queue.remove(session_id)
-			if self.__session_id != session_id: raise AssertionError # False
-			from gobject import idle_add
-			emit = lambda signal: idle_add(self.__manager.emit, signal, data)
-			emit("saved?") if len(data) == 3 else emit("error")
-		except ValueError:
-			print "Module Name: SCRIBES/SaveSystem/SessionCompleter"
-			print "Method Name: __verify_session"
-			print "ERROR MESSAGE: Session id not in queue", session_id
-		except AssertionError:
-			print "Module Name: SCRIBES/SaveSystem/SessionCompleter"
-			print "Method Name: __verify_session"
-			print "ERROR: Session id has changed!"
-			print "Old session id: %s, New session id: %s" % (self.__session_id, session_id)
+		from gobject import idle_add
+		emit = lambda signal: idle_add(self.__manager.emit, signal, data)
+		emit("saved?") if len(data) == 3 else emit("error")
 		return False
 
 	def __destroy(self):
@@ -47,11 +30,6 @@ class Completer(SignalManager):
 	def __quit_cb(self, *args):
 		from gobject import idle_add
 		idle_add(self.__destroy)
-		return False
-
-	def __session_cb(self, manager, session_id):
-		self.__session_id = session_id
-		self.__queue.append(session_id)
 		return False
 
 	def __data_cb(self, manager, data):
