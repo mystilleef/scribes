f61ca95333d1d4614a7abab3cc27fd4e7f1ea16b
Lateef Alabi-Oki <mystilleef@gmail.com>
2011-11-10 08:52:00 -0500

    Implement new module for managing GObject timers correctly and safely.

---
+++ SCRIBES/GObjectTimerManager.py
@@ -0,0 +1,35 @@
+class Manager(object):
+
+	def __init__(self):
+		from collections import deque
+		self.__timers = deque()
+		print "Initialized GObject Timer Manager"
+
+	def add(self, timer):
+		self.__timers.append(timer)
+		print "Added new GObject timer"
+		return False
+
+	def remove(self, timer):
+		if timer not in self.__timers: return False
+		self.__timers.remove(timer)
+		from gobject import source_remove
+		source_remove(timer)
+		print "Removed Gobject timer ", timer
+		return False
+
+	def delete(self, timer):
+		return self.remove(timer)
+
+	def remove_all(self):
+		from copy import copy
+		[self.remove(timer) for timer in copy(self.__timers)]
+		return False
+
+	def destroy(self):
+		self.remove_all()
+		self.__timers.clear()
+		del self.__timers
+		del self
+		print "Destroyed GObject Timer Manager"
+		return False
