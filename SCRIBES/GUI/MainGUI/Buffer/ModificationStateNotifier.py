1dd901d064fdb5908d6ab3ce8fb8fdfe6e34acdd
Lateef Alabi-Oki <mystilleef@gmail.com>
2011-11-11 10:19:37 -0500

    Clean up modification state notifier.

--- SCRIBES/GUI/MainGUI/Buffer/ModificationStateNotifier.py
+++ SCRIBES/GUI/MainGUI/Buffer/ModificationStateNotifier.py
@@ -1,12 +1,15 @@
-class Notifier(object):
+from SCRIBES.SignalConnectionManager import SignalManager
+
+class Notifier(SignalManager):
 
 	def __init__(self, editor):
+		SignalManager.__init__(self)
 		self.__init_attributes(editor)
-		self.__sigid1 = editor.connect("quit", self.__quit_cb)
-		self.__sigid2 = editor.connect("checking-file", self.__block_cb)
-		self.__sigid3 = editor.connect("loaded-file", self.__unblock_cb)
-		self.__sigid4 = editor.connect("load-error", self.__unblock_cb)
-		self.__sigid5 = self.__buffer.connect("modified-changed", self.__modified_cb)
+		self.connect(editor, "quit", self.__quit_cb)
+		self.connect(editor, "checking-file", self.__block_cb)
+		self.connect(editor, "loaded-file", self.__unblock_cb)
+		self.connect(editor, "load-error", self.__unblock_cb)
+		self.__sigid1 = self.connect(self.__buffer, "modified-changed", self.__modified_cb)
 		editor.register_object(self)
 
 	def __init_attributes(self, editor):
@@ -15,41 +18,26 @@ class Notifier(object):
 		return
 
 	def __destroy(self):
-		self.__editor.disconnect_signal(self.__sigid1, self.__editor)
-		self.__editor.disconnect_signal(self.__sigid2, self.__editor)
-		self.__editor.disconnect_signal(self.__sigid3, self.__editor)
-		self.__editor.disconnect_signal(self.__sigid4, self.__editor)
-		self.__editor.disconnect_signal(self.__sigid5, self.__buffer)
+		self.disconnect()
 		self.__editor.unregister_object(self)
 		del self
-		self = None
-		return False
-
-	def __emit(self):
-		modified = self.__buffer.get_modified()
-		self.__editor.emit("modified-file", modified)
-		return False
-
-	def __block_signal(self):
-		self.__buffer.handler_block(self.__sigid5)
-		return False
-
-	def __unblock_signal(self):
-		self.__buffer.handler_unblock(self.__sigid5)
 		return False
 
 	def __quit_cb(self, *args):
-		self.__destroy()
+		from gobject import idle_add
+		idle_add(self.__destroy)
 		return False
 
 	def __block_cb(self, *args):
-		self.__block_signal()
+		self.__buffer.handler_block(self.__sigid1)
 		return False
 
 	def __unblock_cb(self, *args):
-		self.__unblock_signal()
+		self.__buffer.handler_unblock(self.__sigid1)
 		return False
 
 	def __modified_cb(self, *args):
-		self.__emit()
+		modified = self.__buffer.get_modified()
+		from gobject import idle_add
+		idle_add(self.__editor.emit, "modified-file", modified)
 		return False
